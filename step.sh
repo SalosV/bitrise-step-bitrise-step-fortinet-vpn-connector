d84c78aba99b6ae6168e414469158f02a5cb51e5
santiagolozada <santiago.lozada@rappi.com>
2024-01-04 23:14:38 -0500

    vpn step

--- step.sh
+++ step.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
-set -ex
+set -x
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+  echo "Add yarn public keys"
+  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+  echo "Update repositories, installing ppp and openfortivpn"
+  sudo apt-get update && sudo apt-get install -y ppp && sudo apt-get install -y openfortivpn
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+else
 
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+  echo "Installing openfortivpn on MacOS"
+  brew install -y openfortivpn
+
+fi
+
+echo "Starting VPN connection with gateways - ${host}:${port}"
+sudo nohup openfortivpn ${host}:${port} --password=${password} --username=${username} --trusted-cert ${trusted_cert} &> $BITRISE_DEPLOY_DIR/logs.txt &
+
+echo "Waiting connection"
+NUMBER_OF_RETRY=0
+until fgrep -q "Tunnel is up" $BITRISE_DEPLOY_DIR/logs.txt || [ $NUMBER_OF_RETRY -eq 25 ]; do
+  ((NUMBER_OF_RETRY++))
+  cat $BITRISE_DEPLOY_DIR/logs.txt
+  sleep 1;
+done
